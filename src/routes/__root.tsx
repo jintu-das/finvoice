b529474bfccbadd50d6835310dfede1ee00755a3
Jintu Das <jintudas047@gmail.com>
2025-08-08 19:24:46 +0530

    feat: add DropdownMenu component and integrate it into AppSidebar for enhanced user interaction

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,12 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarTrigger, SidebarProvider } from "@/components/ui/sidebar";
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <SidebarProvider>
+      <AppSidebar />
       <div className="p-2 flex gap-2">
         <Link to="/" className="[&.active]:font-bold">
           Home
@@ -13,8 +16,11 @@ export const Route = createRootRoute({
         </Link>
       </div>
       <hr />
-      <Outlet />
+      <main>
+        <SidebarTrigger />
+        <Outlet />
+      </main>
       <TanStackRouterDevtools />
-    </>
+    </SidebarProvider>
   ),
 });
