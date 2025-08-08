b9247b53df649aa0b94ab63501f476b7c0aa1cde
Jintu Das <jintudas047@gmail.com>
2025-08-08 20:11:50 +0530

    feat: implement SignUpForm component and integrate it into sign-up route; update LoginForm to use Link for navigation

--- src/features/authentication/components/signup-form.tsx
+++ src/features/authentication/components/signup-form.tsx
@@ -4,19 +4,24 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Link } from "@tanstack/react-router";
 
-export function LoginForm({
+export function SignUpForm({
   className,
   ...props
 }: React.ComponentProps<"form">) {
   return (
     <form className={cn("flex flex-col gap-6", className)} {...props}>
       <div className="flex flex-col items-center gap-2 text-center">
-        <h1 className="text-2xl font-bold">Login to your account</h1>
-        <p className="text-muted-foreground text-sm text-balance">
-          Enter your email below to login to your account
+        <h1 className="text-2xl font-bold">Create an Account</h1>
+        <p className="text-muted-foreground text-sm ">
+          Join now to start managing your invoices and finances with ease.
         </p>
       </div>
       <div className="grid gap-6">
+        <div className="grid gap-3">
+          <Label htmlFor="name">Name</Label>
+          <Input id="name" placeholder="Dwight Schrute" required />
+        </div>
+
         <div className="grid gap-3">
           <Label htmlFor="email">Email</Label>
           <Input
@@ -27,19 +32,20 @@ export function LoginForm({
           />
         </div>
         <div className="grid gap-3">
-          <div className="flex items-center">
-            <Label htmlFor="password">Password</Label>
-            <a
-              href="#"
-              className="ml-auto text-sm underline-offset-4 hover:underline"
-            >
-              Forgot your password?
-            </a>
-          </div>
-          <Input id="password" type="password" required />
+          <Label htmlFor="password">Password</Label>
+          <Input id="password" type="password" placeholder="******" required />
+        </div>
+        <div className="grid gap-3">
+          <Label htmlFor="confirm-password">Confirm Password</Label>
+          <Input
+            id="confirm-password"
+            type="password"
+            placeholder="******"
+            required
+          />
         </div>
         <Button type="submit" className="w-full">
-          Login
+          Register
         </Button>
         <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
           <span className="bg-background text-muted-foreground relative z-10 px-2">
@@ -57,9 +63,9 @@ export function LoginForm({
         </Button>
       </div>
       <div className="text-center text-sm">
-        Don&apos;t have an account?{" "}
-        <Link to="/sign-up" className="underline underline-offset-4">
-          Sign up
+        Already have an account?{" "}
+        <Link to="/sign-in" className="underline underline-offset-4">
+          Sign In
         </Link>
       </div>
     </form>
