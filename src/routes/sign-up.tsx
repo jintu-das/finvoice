daa4fe7302954521321f66f464bf571083e086a2
Jintu Das <jintudas047@gmail.com>
2025-08-08 20:38:56 +0530

    feat: update color variables in CSS and replace anchor tags with Link components in sign-in and sign-up routes for improved navigation

--- src/routes/sign-up.tsx
+++ src/routes/sign-up.tsx
@@ -1,5 +1,5 @@
 import { SignUpForm } from "@/features/authentication/components/signup-form";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Link } from "@tanstack/react-router";
 import { FileText } from "lucide-react";
 
 export const Route = createFileRoute("/sign-up")({
@@ -11,12 +11,12 @@ function RouteComponent() {
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
+          <Link to="/" className="flex items-center gap-2 font-medium">
             <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
               <FileText className="size-4" />
             </div>
             Finvoice
-          </a>
+          </Link>
         </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-sm">
