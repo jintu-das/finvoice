9bbaf000430ac805394e4480ce080fc93a14f006
Jintu Das <jintudas047@gmail.com>
2025-08-08 20:35:23 +0530

    feat: update max-width of form containers in sign-in and sign-up routes for improved layout

--- src/routes/sign-up.tsx
+++ src/routes/sign-up.tsx
@@ -19,7 +19,7 @@ function RouteComponent() {
           </a>
         </div>
         <div className="flex flex-1 items-center justify-center">
-          <div className="w-full max-w-xs">
+          <div className="w-full max-w-sm">
             <SignUpForm />
           </div>
         </div>
