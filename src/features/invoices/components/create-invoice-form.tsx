1b19b10d3a08d16bc5bdea2567d578c4ddde396d
Jintu Das <jintudas047@gmail.com>
2025-08-15 21:01:03 +0530

    feat: implement invoice form schema and enhance CreateInvoiceForm with item management

--- src/features/invoices/components/create-invoice-form.tsx
+++ src/features/invoices/components/create-invoice-form.tsx
@@ -1,5 +1,5 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
 import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
@@ -13,42 +13,39 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-
-const formSchema = z.object({
-  dueDate: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-  issueDate: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-  client: z.string().min(2, {
-    message: "Client name must be at least 2 characters.",
-  }),
-  note: z.string().optional(),
-});
+import { Plus } from "lucide-react";
+import { invoiceFormSchema } from "../schemas";
 
 export function CreateInvoiceForm() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof invoiceFormSchema>>({
+    resolver: zodResolver(invoiceFormSchema),
     defaultValues: {
       dueDate: "",
       issueDate: "",
       client: "",
       note: "",
+      items: [{ description: "", quantity: 1, price: 0 }],
     },
   });
 
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
+  const { append, fields } = useFieldArray({
+    control: form.control,
+    name: "items",
+  });
+
+  function onSubmit(values: z.infer<typeof invoiceFormSchema>) {
     console.log(values);
   }
+
+  function appendItem() {
+    append({ description: "", quantity: 1, price: 0 });
+  }
+
   return (
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className="space-y-8 max-w-md grid grid-cols-2 gap-4 items-start pt-8"
+        className="space-y-8 max-w-lg grid grid-cols-2 gap-4 items-start pt-8"
       >
         <FormField
           control={form.control}
@@ -109,6 +106,69 @@ export function CreateInvoiceForm() {
             </FormItem>
           )}
         />
+
+        <div className="col-span-2 ">
+          {fields.map((item, index) => (
+            <div
+              className="grid grid-cols-[1fr_90px_90px] items-start gap-3"
+              key={item.id}
+            >
+              <FormField
+                control={form.control}
+                name={`items.${index}.description`}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Description</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name={`items.${index}.quantity`}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Quantity</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="number"
+                        {...field}
+                        onChange={(e) => field.onChange(e.target.valueAsNumber)}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name={`items.${index}.price`}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Price</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="number"
+                        {...field}
+                        onChange={(e) => field.onChange(e.target.valueAsNumber)}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          ))}
+          <Button variant="secondary" onClick={appendItem} className="mt-4">
+            <Plus className="size-4" /> Add Item
+          </Button>
+        </div>
+
         <div>
           <Button type="submit">Submit</Button>
         </div>
---
+++ src/features/invoices/schemas.ts
@@ -0,0 +1,27 @@
+import z from "zod";
+
+export const invoiceFormSchema = z.object({
+  dueDate: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  issueDate: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  client: z.string().min(2, {
+    message: "Client name must be at least 2 characters.",
+  }),
+  note: z.string().optional(),
+  items: z.array(
+    z.object({
+      description: z.string().min(1, {
+        message: "Description is required.",
+      }),
+      quantity: z.number().min(1, {
+        message: "Quantity must be at least 1.",
+      }),
+      price: z.number().min(0.01, {
+        message: "Price must be at least 0.01.",
+      }),
+    })
+  ),
+});
